f0b79722b18b452d10694e67566ad30a58abc803
Dan Imbrogno <dan@pmlfreight.tech>
2019-05-30 15:34:21 -0400

    ci: conditional canary release

--- build/build-container.sh
+++ build/build-container.sh
@@ -10,7 +10,7 @@ set -o errexit
 set -o pipefail
 
 PACKAGE=$1
-PACKAGE_DIST_TAG=$2
+IS_CANARY=$2
 
 cd ./packages/$PACKAGE
 
@@ -26,23 +26,31 @@ IMAGE_DIST_TAG="pickmyload/test-$PACKAGE:$PACKAGE_DIST_TAG"
 echo "THE IMAGE TAG IS $IMAGE_DIST_TAG"
 echo "THE IMAGE COMMIT TAG IS $IMAGE_TAG"
 
-echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_LOGIN --password-stdin
-
-
-# If container already exists with this tag
-if DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect $IMAGE_TAG >/dev/null; then
-    
-  echo "skipping build of docker image $IMAGE_TAG. It already exists."
-
-else
+deploy () {
     
     echo "building docker image $IMAGE_TAG"
+
     docker build --build-arg PACKAGE_VERSION=$PACKAGE_VERSION --build-arg NPM_TOKEN=$NPM_TOKEN -t $IMAGE_TAG -t $IMAGE_DIST_TAG .
 
     docker push $IMAGE_TAG
     docker push $IMAGE_DIST_TAG
 
+}
 
+testExists () {
+    DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect $IMAGE_TAG >/dev/null
+}
+
+echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_LOGIN --password-stdin
+
+# If we are doing canary release
+if [ "$IS_CANARY" -eq "1" ]; then
+    deploy
+# Or the image does not exist in the repository
+elif ! testExists ; then
+    deploy
+else
+    echo "skipping build of docker image $IMAGE_TAG. It already exists."
 fi
 
 docker logout
--- packages/a/src/index.ts
+++ packages/a/src/index.ts
@@ -1,3 +1,3 @@
 import foo from '@freightbpi/testing-b';
 
-console.log(`My address is: ${foo()}`);
+console.log(`My address is ${foo()}!`);
