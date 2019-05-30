147d2f50d6dc1bea31d58c5234ca23f485cfaa09
Dan Imbrogno <dan@pmlfreight.tech>
2019-05-30 13:39:16 -0400

    ci: tag docker images with sha1 and appropriate version

--- build/build-container.sh
+++ build/build-container.sh
@@ -9,21 +9,29 @@ set -o errexit
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
-cd ./packages/$1
+PACKAGE=$1
+PACKAGE_VERSION=$2
 
-PACKAGE_VERSION="$(awk -F\" '/"version":/ {print $4}' package.json)"
+cd ./packages/$PACKAGE
 
+#PACKAGE_VERSION="$(awk -F\" '/"version":/ {print $4}' package.json)"
+
+echo "THE PACKAGE IS $PACKAGE"
 echo "THE PACKAGE VERSION IS $PACKAGE_VERSION"
 
-IMAGE_TAG="pickmyload/test-$1:next"
+IMAGE_COMMIT_TAG="pickmyload/test-$PACKAGE:$CIRCLE_SHA1"
+IMAGE_TAG="pickmyload/test-$PACKAGE:$PACKAGE_VERSION"
 
 echo "THE IMAGE TAG IS $IMAGE_TAG"
+echo "THE IMAGE COMMIT TAG IS $IMAGE_COMMIT_TAG"
 
 echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_LOGIN --password-stdin
 
-docker build --build-arg PACKAGE_VERSION=$PACKAGE_VERSION --build-arg NPM_TOKEN=$NPM_TOKEN -t $IMAGE_TAG .
+docker build --build-arg PACKAGE_VERSION=$PACKAGE_VERSION --build-arg NPM_TOKEN=$NPM_TOKEN -t $IMAGE_COMMIT_TAG -t $IMAGE_TAG .
 
+docker push $IMAGE_COMMIT_TAG
 docker push $IMAGE_TAG
+
 
 # If container already exists with this tag
 # if DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect $IMAGE_TAG >/dev/null; then
